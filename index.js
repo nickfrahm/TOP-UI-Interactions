55535001af64a5c2261275ece46837e876b3a443
nickfrahm <nfrahm24@gmail.com>
2021-07-15 22:25:08 -0500

    thinking through how i want to make this reusable

---
+++ index.js
@@ -0,0 +1,8 @@
+/*
+Find the element that I need to show the dropdown for, 
+then call a reusable function to grab the hidden list assigned to the item and show it.
+*/
+
+document.querySelectorAll('.link').forEach((link) => {
+  //link.querySelector('.dropdown').dropDownMenu();
+});
