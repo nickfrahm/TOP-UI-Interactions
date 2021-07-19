16ead408e7482a39653555c8e6dd42a7cbd1d7f5
nickfrahm <nfrahm24@gmail.com>
2021-07-18 20:34:23 -0500

    working along basic ui interactions still

--- index.js
+++ index.js
@@ -3,6 +3,33 @@ Find the element that I need to show the dropdown for,
 then call a reusable function to grab the hidden list assigned to the item and show it.
 */
 
-document.querySelectorAll('.link').forEach((link) => {
-  //link.querySelector('.dropdown').dropDownMenu();
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('.link').forEach((link) => {
+    link.addEventListener('mouseover', (e) => {
+      showDropDownMenu(e);
+    });
+    link.addEventListener('mouseleave', (e) => {
+      hideDropDownMenu(e);
+    });
+  });
+
+  document.querySelector('.menu-icon').addEventListener('click', (e) => {
+    showMobileMenu(e);
+  });
 });
+
+function showDropDownMenu(link) {
+  if (link.target.classList.contains('link')) {
+    link.target.querySelector('.dropdown').style.display = 'block';
+  }
+}
+
+function hideDropDownMenu(link) {
+  if (link.target.classList.contains('link')) {
+    link.target.querySelector('.dropdown').style.display = 'none';
+  }
+}
+
+function showMobileMenu(icon) {
+  document.querySelector('.mobile');
+}
